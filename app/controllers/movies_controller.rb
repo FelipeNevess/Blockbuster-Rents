7726e34774971285d8354b0f18388e434be9b3c6
felipenevess <felipe012neves@gmail.com>
2022-06-24 18:34:46 -0300

    feat: criando scaffold de Movie

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,49 @@
+class MoviesController < ApplicationController
+  before_action :set_movie, only: %i[show edit update destroy]
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show; end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def edit; end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to movie_url(@movie), notice: 'Filme criado com sucesso.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @movie.update(movie_params)
+      redirect_to movie_url(@movie), notice: 'Filme atualizado com sucesso.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie.destroy
+
+    redirect_to movies_url, notice: 'Filme removido com sucesso.'
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :release_year, :duration, :genre, :classification, :description)
+  end
+end
---
+++ app/models/movie.rb
@@ -0,0 +1,11 @@
+class Movie < ApplicationRecord
+  validates :title,
+            :release_year,
+            :duration,
+            :classification,
+            :description, presence: true
+  validates :title, length: { minimum: 2, maximum: 50 }
+  validates :release_year, numericality: true
+  validates :duration, length: { minimum: 5,  maximum: 20 }
+  validates :description, length: { minimum: 50 }
+end
