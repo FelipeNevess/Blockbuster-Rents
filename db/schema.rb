ed5f19f7ee7809ede91d24c78777b86bafc03c4b
felipenevess <felipe012neves@gmail.com>
2022-06-24 18:35:26 -0300

    feat: criando migração de Movie

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_22_235649) do
+ActiveRecord::Schema.define(version: 2022_06_24_211323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "release_year", null: false
+    t.string "duration", null: false
+    t.string "classification", null: false
+    t.text "description", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
