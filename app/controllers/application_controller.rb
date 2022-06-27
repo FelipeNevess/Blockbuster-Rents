2ef6bde69799831d2b7afa8243200a1a82d44347
felipenevess <felipe012neves@gmail.com>
2022-06-27 16:11:01 -0300

    feat: dando permições para name e avatar (devise)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[avatar name])
   end
 end
