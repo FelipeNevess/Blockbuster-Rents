5d390638b7c1dcf1e6bcce00102c023c74d3c66b
felipenevess <felipe012neves@gmail.com>
2022-06-27 16:47:39 -0300

    feat: adicionando relacionamento com active storage (avatar)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise           :database_authenticatable,
+                   :registerable,
+                   :recoverable,
+                   :rememberable,
+                   :validatable
 
-  validates :name, presence: true
+  validates        :name, presence: true
+
+  has_one_attached :avatar
+  validates        :avatar, content_type: %i[png jpg jpeg]
+
+  def avatar_thumbnail
+    if avatar.attached?
+      avatar.variant(resize: '50x50^', extent: '50x50')
+    else
+      'user.svg'
+    end
+  end
 end
