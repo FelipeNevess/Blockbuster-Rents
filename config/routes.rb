7593b92c2564ac59e468eeec888a0548cb31f36b
felipenevess <felipe012neves@gmail.com>
2022-07-01 20:05:50 -0300

    feat: editando nome das rotas do devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'movies#index'
 
-  devise_for :users, only: %i[sessions registrations passwords]
+  devise_for :users, only: %i[sessions registrations passwords], path: '', path_names: {
+    sign_in: 'session/login',
+    registration: 'session/simple-register',
+    sign_up: ''
+  }
 
   resources :movies
 end
