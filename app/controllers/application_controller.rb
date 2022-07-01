7b04342b72a3218d8b60154bde4e8121a391ce28
felipenevess <felipe012neves@gmail.com>
2022-07-01 11:47:37 -0300

    feat: redirecionando layout de authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: %i[index show]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  layout :layout_by_resource
+
+  private
+
+  def layout_by_resource
+    'authentication' if devise_controller?
+  end
+
   protected
 
   def configure_permitted_parameters
