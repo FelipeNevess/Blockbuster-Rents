ca4517301964f84bdece13eef96208c93ad7e5ec
felipenevess <felipe012neves@gmail.com>
2022-07-03 15:26:00 -0300

    feat: adicionando metodo de update password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def update_password
+    if current_user.update(user_params)
+      redirect_to root_path, notice: 'Senha atualizada com sucesso.'
+    else
+      flash.now[:alert] = current_user.errors.full_messages.to_sentence
+
+      redirect_to accounts_register_path
+    end
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
