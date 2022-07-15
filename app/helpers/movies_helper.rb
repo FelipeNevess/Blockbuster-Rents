8eb87fce58f6a3ffb090029e32edfea7125cb5da
felipenevess <felipe012neves@gmail.com>
2022-07-15 20:08:55 -0300

    feat: adicionando helper movies

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,5 +1,5 @@
 module MoviesHelper
   def search_movie_by_category(category)
-    Category.find(category.id).movies.distinct if Category.exists?(category.id)
+    Category.find(category).movies.distinct.first(20) if Category.exists?(category)
   end
 end
