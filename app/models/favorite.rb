94601f8817de8870263de33f9173a162a66fb516
felipenevess <felipe012neves@gmail.com>
2022-07-19 15:16:52 -0300

    feat: criando validação de like unico

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,6 @@
 class Favorite < ApplicationRecord
   belongs_to :movie
   belongs_to :user
+
+  validates  :movie_id, uniqueness: { scope: :user_id, message: 'Esse filme já está em favoritado' }
 end
