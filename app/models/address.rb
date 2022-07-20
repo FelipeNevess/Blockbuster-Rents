e134b2d21bbeda5952587df22ad7c1f66d332ad1
felipenevess <felipe012neves@gmail.com>
2022-07-20 16:26:49 -0300

    feat: criando validações de endereço

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,10 @@
 class Address < ApplicationRecord
   validates   :number, uniqueness: true
+  validates   :number,
+              :zip_code,
+              :city,
+              :state,
+              :street, presence: { message: 'deve estar presente' }
 
   belongs_to  :user
 end
