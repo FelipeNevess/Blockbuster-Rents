b31d1040015a0b622753b9ad0001415a8943b2ba
felipenevess <felipe012neves@gmail.com>
2022-07-22 21:44:41 -0300

    feat: criando metodo de renomear valores de categoria

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,13 @@
 class Category < ApplicationRecord
   has_many :movie_categories
   has_many :movies, through: :movie_categories
+
+  def category_by(category)
+    case category
+    when 'Western' then 'Filmes de Bang Bang!'
+    when 'Infantil' then 'EU TÔ DE FÉRIAS!'
+    else
+      category
+    end
+  end
 end
