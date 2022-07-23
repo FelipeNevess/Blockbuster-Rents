b2e949b01033d55ca2421da5bafd189cb486906e
felipenevess <felipe012neves@gmail.com>
2022-07-22 21:43:41 -0300

    refactor: reajustando metodo search_movie_by_category

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,5 +1,7 @@
 module MoviesHelper
   def search_movie_by_category(category)
-    Category.find(category).movies.distinct.first(12) if Category.exists?(category)
+    movies = Category.find(category).movies
+
+    movies.where.not("card_image ILIKE '%/ban/%'").order(title: :desc, description: :desc)
   end
 end
