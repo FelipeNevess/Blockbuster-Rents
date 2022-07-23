3accdbe814ea88f38841479c5cbd3de4afcf2d05
felipenevess <felipe012neves@gmail.com>
2022-07-22 21:42:04 -0300

    feat: ajustando o metodo set_movie_category e adicionando set_movie_popular

--- app/controllers/concerns/options_categories.rb
+++ app/controllers/concerns/options_categories.rb
@@ -5,10 +5,48 @@ module OptionsCategories
 
   def set_movie_category
     @categories = Category.where(
+      category: ['Ação', 'Aventura', 'Drama', 'Romance', 'Crime',
+                 'Suspense', 'Terror', 'Animção', 'Documentários',
+                 'Clássicos', 'Comédia', 'Western', 'Ficção Científica']
+    ).order(category: :asc)
+  end
+
+  def set_movie_popular
+    @popularity = Category.where(
       category: %w[
-        Ação Aventura Drama Romance Crime
-        Suspense Terror Animção Documentários
+        Policial Mistério Fantasia Biografia
+        Infantil História Família
+        Independente Musical
+        Curta-Metragem Esporte
       ]
-    )
+    ).order(category: :asc)
+
+    # 'Classicos',
+    # 'Comédia',
+    # 'Documentarios',
+    # 'Drama',
+    # 'Ficcao-cientifica',
+    # 'Infantil',
+    # 'Policial',
+    # 'Romance',
+    # 'Suspense',
+    # 'Terror',
+    # 'Faroeste',
+    # 'Novidades',
+    # 'Em-clima-de-ferias',
+    # 'Eu-to-de-ferias',
+    # 'Recomendados',
+    # 'Cinema-nacional',
+    # 'Amizade-animal',
+    # 'Animacao',
+    # 'Classicos-inesqueciveis-',
+    # 'Baseados-em-historias-reais',
+    # 'Comedia-romantica',
+    # 'Premiados',
+    # 'Especial-mazzaropi',
+    # 'Especial-ze-do-caixao',
+    # 'Para-toda-familia',
+    # 'Protagonistas-mulheres',
+    # 'Series'
   end
 end
