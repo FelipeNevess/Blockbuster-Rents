7b739e9bffd359d59aa4d4e022ea850047802874
felipenevess <felipe012neves@gmail.com>
2022-07-25 17:46:22 -0300

    feat: definindo limite de filmes renderizados na tela

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -2,6 +2,6 @@ module MoviesHelper
   def search_movie_by_category(category)
     movies = Category.find(category).movies
 
-    movies.where.not("card_image ILIKE '%/ban/%'").order(title: :desc, description: :desc)
+    movies.where.not("card_image ILIKE '%/ban/%'").order(title: :desc, description: :desc).first(15)
   end
 end
